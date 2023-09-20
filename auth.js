950f26377e1273397901d7dd6b5326575bed4cad
thompson-stephen113 <thompson.stephen113@gmail.com>
2023-09-20 15:17:56 -0400

    created authentication logic
    Authenticates registered users when they log in using their username and password.
    Generates a JWT that will authenticate future requests.

---
+++ auth.js
@@ -0,0 +1,37 @@
+const jwtSecret = "your_jwt_secret";
+
+const jwt = require("jsonwebtoken"),
+    passport = require("passport");
+
+require("./passport");
+
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username,     // Username to be encoded in the JWT
+        expiresIn: "7d",            // Specifies the token will expire in 7 days
+        algorithm: "HS256"          // Algorithm used to sign the values of the JWT
+    });
+}
+
+
+// POST login //
+module.exports = (router) => {
+    router.post("/login", (req, res) => {
+        passport.authenticate("local", { session: false }, (err, user, info) => {
+            if (err || !user) {
+                return res.status(400).json({
+                    message: "Something is not right",
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (err) => {
+                if (err) {
+                    res.send(err);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
