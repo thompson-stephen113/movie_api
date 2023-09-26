5bf5233f230b9c53d3e6afce72d909b04bda5115
Stephen Thompson <thompson.stephen113@gmail.com>
2023-09-26 14:22:56 -0400

    reverting troubleshooting changes

--- index.js
+++ index.js
@@ -134,20 +134,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //------------------------CORS Integration--------------------------//
 const cors = require("cors");
 
-app.use(cors());
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
 
-// let allowedOrigins = ["http://localhost:8080", "https://myflix-db-app-24338506cd5a.herokuapp.com/"];
-
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if(!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(origin) === -1){
-//             let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-//             return callback(new Error(message ), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 
 //--------------------------File Imports----------------------------//
