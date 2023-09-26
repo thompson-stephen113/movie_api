3020d715c172cd487d32f6fc2461ab8b93352c3b
Stephen Thompson <thompson.stephen113@gmail.com>
2023-09-26 10:33:37 -0400

    fixed an error

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const Users = Models.User;
 // mongoose.connect("mongodb://127.0.0.1:27017/myFlix_db", { useNewUrlParser: true, useUnifiedTopology: true});
 
 // Online connection
-mongoose.connect("process.env.CONNECTION_URI", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 // Data for top 10 movies list
