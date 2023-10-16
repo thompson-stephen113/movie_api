d9b2eed083ae0557d531260d6e9a2c27f6616407
Stephen Thompson <thompson.stephen113@gmail.com>
2023-10-16 09:37:33 -0400

    added localhost:1234 to allowedOrigins

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //------------------------CORS Integration--------------------------//
 const cors = require("cors");
 
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "http://testsite.com"];
 
 app.use(cors({
     origin: (origin, callback) => {
